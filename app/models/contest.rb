16eb24ebe02b6513eca0a6f1c2a5ca124fb43f64
Yihang Ho <hoyihang5@gmail.com>
2014-07-19 11:50:56 +0800

    Implement validations for Contest.

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,3 +1,12 @@
 class Contest < ActiveRecord::Base
+  validates :title, :start, :end, :presence => true
+  validate :starting_and_ending_time_must_make_sense
+
   has_and_belongs_to_many :problems
+
+  def starting_and_ending_time_must_make_sense
+    if self.start && self.end && self.start > self.end
+      errors.add(:start, "can't happen after end")
+    end
+  end
 end

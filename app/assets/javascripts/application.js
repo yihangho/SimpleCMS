4467e712b2e3a5d804d2aed03f7303f11991e370
Yihang Ho <hoyihang5@gmail.com>
2014-07-21 19:44:53 +0800

    Add a navbar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap/collapse
+//= require bootstrap/transition
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'signin' => 'sessions#new'
+  get 'signin' => 'sessions#new', :as => 'signin'
   resources :problems, :only => [:index, :new, :create, :show]
   resources :users, :only => [:new, :create]
   resources :sessions, :only => :create

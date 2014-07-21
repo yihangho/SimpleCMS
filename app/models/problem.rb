9837f68d44ec1e80eb01be68f1891a63b1af8ee9
Yihang Ho <hoyihang5@gmail.com>
2014-07-21 23:08:00 +0800

    Implement Problem#cache_solved_status.

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -4,4 +4,16 @@ class Problem < ActiveRecord::Base
   has_many :tasks, :dependent => :destroy
   has_and_belongs_to_many :contests
   has_and_belongs_to_many :solvers, :class_name => "User", :join_table => "solved_problems"
+
+  def solved_by?(user)
+    tasks.all? { |task| task.solved_by?(user) }
+  end
+
+  def cache_solved_status(user)
+    if solved_by?(user)
+      self.solvers |= [user]
+    else
+      self.solvers.delete(user)
+    end
+  end
 end

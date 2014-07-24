a3e9548f2d03e07cbca4df4a9f37366f9f7bab37
Yihang Ho <hoyihang5@gmail.com>
2014-07-24 19:29:08 +0800

    And submissions index now shows all submissions from everyone.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,8 +1,10 @@
 class SubmissionsController < ApplicationController
-  before_action :signed_in, :only => [:index, :create]
+  before_action :admin_only, :only => :index
+  before_action :signed_in, :only => :create
   before_action :correct_user_or_admin, :only => :user
 
   def index
+    @submissions = Submission.order(:created_at => :desc)
   end
 
   def create
@@ -25,9 +27,14 @@ class SubmissionsController < ApplicationController
 
   def user
     @submissions = User.find(params[:id]).submissions.order(:created_at => :desc)
+    render 'index'
   end
 
   private
+
+  def admin_only
+    redirect_to problems_path unless signed_in? && current_user.admin?
+  end
 
   def submission_params
     params.require(:submission).permit(:task_id, :input, :code_link)

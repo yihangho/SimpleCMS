c176bbff2b5358c94c1a45bbda780b5c549b6694
Yihang Ho <hoyihang5@gmail.com>
2014-07-24 14:56:51 +0800

    Navbar will now have some admin-specific items.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap/alert
 //= require bootstrap/collapse
+//= require bootstrap/dropdown
 //= require bootstrap/transition
 //= require turbolinks
 //= require_tree .

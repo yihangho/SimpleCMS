85bd0f8795de946ac2bdc4873b39b6ff936c20d6
Yihang Ho <hoyihang5@gmail.com>
2014-07-24 17:38:19 +0800

    Remove remember_token from User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724092732) do
+ActiveRecord::Schema.define(version: 20140724093702) do
 
   create_table "contests", force: true do |t|
     t.string   "title"
@@ -115,13 +115,11 @@ ActiveRecord::Schema.define(version: 20140724092732) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_token"
     t.boolean  "admin",           default: false
     t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["name"], name: "index_users_on_name"
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token", unique: true
 
 end

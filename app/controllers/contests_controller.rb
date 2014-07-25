ce6d89bd8a401b87dd50df26d34fea9f4311e1d4
Yihang Ho <hoyihang5@gmail.com>
2014-07-25 23:22:54 +0800

    The invited contests list should list only upcoming contests.

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -55,7 +55,7 @@ class ContestsController < ApplicationController
   end
 
   def invited
-    @contests = Contest.invited_but_not_participated_by(current_user)
+    @contests = Contest.invited_but_not_participated_by(current_user).upcoming
     render 'index'
   end
 

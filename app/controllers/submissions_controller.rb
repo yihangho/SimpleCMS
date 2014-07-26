ac168431255e9da2e45c707424a262305402e455
Yihang Ho <hoyihang5@gmail.com>
2014-07-26 12:06:24 +0800

    Ensure that only owner or admin can view individual submissions.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,6 +2,7 @@ class SubmissionsController < ApplicationController
   before_action :admin_only, :only => :index
   before_action :signed_in_users_only, :only => :create
   before_action :correct_user_or_admin, :only => :user
+  before_action :owner_or_admin, :only => :show
 
   def index
     @submissions = Submission.order(:created_at => :desc).paginate(:page => params[:page])
@@ -42,6 +43,16 @@ class SubmissionsController < ApplicationController
       redirect_to signin_path
     elsif !(current_user?(User.find(params[:id])) || current_user.admin?)
       flash[:danger] = "You can only view your own submissions."
+      redirect_to user_submissions_path(current_user)
+    end
+  end
+
+  def owner_or_admin
+    if !signed_in?
+      store_location
+      redirect_to signin_path
+    elsif !(current_user?(Submission.find(params[:id]).user) || current_user.admin?)
+      flash[:danger] = "You are not allowed to view this submission."
       redirect_to user_submissions_path(current_user)
     end
   end

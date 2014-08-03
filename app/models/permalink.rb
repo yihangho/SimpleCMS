a56018447d9c8d7f07cfff18506fd03c6a1268ca
Yihang Ho <hoyihang5@gmail.com>
2014-08-03 20:00:51 +0800

    Implement Permalink#to_s.

--- app/models/permalink.rb
+++ app/models/permalink.rb
@@ -1,4 +1,8 @@
 class Permalink < ActiveRecord::Base
   validates :url, :presence => true, :uniqueness => { :case_sensitive => false }
   belongs_to :linkable, :polymorphic => true
+
+  def to_s
+    url
+  end
 end

de39442f2504133998b36eb0f6d449526dd45b45
Yihang Ho <hoyihang5@gmail.com>
2014-08-13 09:32:32 +0800

    Add school column to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808050736) do
+ActiveRecord::Schema.define(version: 20140813012902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,9 +150,11 @@ ActiveRecord::Schema.define(version: 20140808050736) do
     t.datetime "updated_at"
     t.boolean  "admin",           default: false
     t.string   "username"
+    t.string   "school"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["school"], name: "index_users_on_school", using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end

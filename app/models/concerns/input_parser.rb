096292da0df781210012c9644a7ab13e2e807fca
fyquah95 <quah.fy95@gmail.com>
2014-08-14 10:23:13 +0800

    added normalize input

---
+++ app/models/concerns/input_parser.rb
@@ -0,0 +1,12 @@
+module InputParser
+  extend ActiveSupport::Concern
+
+  def self.normalize_input input , space_sensitive = false
+    unless space_sensitive
+      # remove all carriage returns and backspace and tab first
+      arr_of_lines = input.gsub(/[\r\b\t]+/ , "").split("\n").map! { |line| line.gsub(/\s+/ , " ").strip }
+      arr_of_lines.reject! { |line| line.empty? }
+      arr_of_lines.join("\n") << "\n"
+    end
+  end
+end

2ae2d5d383732096f00232f6d3510fedc6f87976
Yihang Ho <hoyihang5@gmail.com>
2014-08-15 14:42:50 +0800

    Write tests for signin.

---
+++ features/signin.feature
@@ -0,0 +1,34 @@
+Feature: Signin
+  In order to perform some restricted actions
+  As a registered user
+  I want signin using my account
+
+  Scenario: Using correct username/password combo
+    Given I am a registered user
+    And   I am visiting "/signin"
+    When  I submit the signin form with my username and password
+    Then  I should be on my profile page
+
+  Scenario: Using correct email/password combo
+    Given I am a registered user
+    And   I am visiting "/signin"
+    When  I submit the signin form with my email and password
+    Then  I should be on my profile page
+
+  Scenario: Using incorrect username
+    Given I am a registered user
+    And   I am visiting "/signin"
+    When  I submit the signin form with incorrect username
+    Then  I should see "Wrong email/username and/or password."
+
+  Scenario: Using incorrect email
+    Given I am a registered user
+    And   I am visiting "/signin"
+    When  I submit the signin form with incorrect email
+    Then  I should see "Wrong email/username and/or password."
+
+  Scenario: Using incorrect password
+    Given I am a registered user
+    And   I am visiting "/signin"
+    When  I submit the signin form with incorrect password
+    Then  I should see "Wrong email/username and/or password."
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -13,6 +13,10 @@ Given(/^An account with (.*?) set to "(.*?)" had been created$/) do |key, value|
   User.create(hash)
 end
 
+Given(/^I am a registered user$/) do
+  @user = User.create(:username => "test", :email => "test@example.com", :password => "12345", :password_confirmation => "12345")
+end
+
 When(/^I submit the user registration form with valid information$/) do
   fill_in 'Username', :with => "test"
   fill_in 'Email', :with => "test@example.com"
@@ -29,10 +33,27 @@ When(/^I submit the user registration form with unmatching password and password
   click_button 'Register'
 end
 
+When(/^I submit the signin form with my (.*?) and password$/) do |identifier|
+  fill_in 'Username/Email', :with => @user.send(identifier.to_sym)
+  fill_in 'Password', :with => @user.password
+  click_button 'Sign In'
+end
+
+When(/^I submit the signin form with incorrect (.*?)$/) do |field|
+  fill_in 'Username/Email', :with => @user.username
+  fill_in 'Password', :with => @user.password
+  fill_in field.capitalize, :with => @user.send(field.to_sym) + "bla"
+  click_button 'Sign In'
+end
+
 Then(/^I should be on "(.*?)"$/) do |path|
   expect(current_path).to eq path
 end
 
 Then(/^I should see "(.*?)"$/) do |content|
   expect(page).to have_content(content)
+end
+
+Then(/^I should be on my profile page$/) do
+  step %(I should be on "#{user_path(@user)}")
 end

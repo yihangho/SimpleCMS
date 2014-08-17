36d9c5df8ded81611a1a798352a395dbe9d14c2a
Yihang Ho <hoyihang5@gmail.com>
2014-08-17 22:04:16 +0800

    Rewrite the problems form using AngularJS.

---
+++ app/assets/javascripts/AngularJS/ProblemForm.js
@@ -0,0 +1,68 @@
+var app = angular.module('ProblemFormApp', []);
+
+app.controller('ProblemFormController', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    // Default values for a new problem
+    $scope.problem = {
+        contest_only: true,
+        permalink_attributes: {},
+        tasks_attributes: []
+    };
+
+    $scope.saveProblem = function($event) {
+        $event.preventDefault();
+
+        if (!$scope.problem.permalink_attributes.url || $scope.problem.permalink_attributes.url.trim().length == 0) {
+            $scope.problem.permalink_attributes["_destroy"] = true;
+        }
+
+        if ($scope.problem.id !== undefined && $scope.problem.id !== null) {
+            var endPoint = "/problems/" + $scope.problem.id + ".json";
+            var savePromise = $http.put(endPoint, $scope.getSubmissionParams());
+        } else {
+            var endPoint = "/problems.json";
+            var savePromise = $http.post(endPoint, $scope.getSubmissionParams());
+        }
+
+        savePromise.success(function(data) {
+            $scope.problem = data;
+
+            if (!data.errors || data.errors.length == 0) {
+                $window.location.pathname = "/problems/" + data.id;
+            }
+        });
+    };
+
+    $scope.getSubmissionParams = function() {
+        return {
+            authenticity_token: $scope.authenticity_token,
+            problem: $scope.problem
+        };
+    };
+
+    $scope.addTask = function() {
+        $scope.problem.tasks_attributes.push({});
+    };
+}]);
+
+app.directive('problemId', ['$http', function($http) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var problemId = attrs['problemId'];
+            if (problemId !== undefined && problemId !== null) {
+                $http.get('/problems/' + problemId + '.json').success(function(data) {
+                    scope.problem = data;
+                });
+            }
+        }
+    };
+}]);
+
+app.directive('authenticityToken', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            scope["authenticity_token"] = attrs['authenticityToken'];
+        }
+    };
+})
--- app/assets/javascripts/tasks.js.coffee
+++ app/assets/javascripts/tasks.js.coffee
@@ -3,15 +3,6 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on "page:load", ->
-  task_count = 0
-
-  $.Mustache.addFromDom("task-template") if $("#task-template").length
-
   $("form").on "click", '[data-dismiss="task-form"]', ->
     if confirm("Are you sure?")
       $($(this).data("target")).remove()
-
-  $("#add-task-btn").click ->
-    task_count++ until($(".task-#{task_count}").length == 0)
-    $(".tasks-forms").append($.Mustache.render("task-template", {task_count: task_count}))
-    return false

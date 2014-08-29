d69b3fab019479feeea439c194a079714a7bb79b
Yihang Ho <hoyihang5@gmail.com>
2014-08-29 11:40:37 +0800

    Ensure the input is a string for InputParser.

--- app/models/concerns/input_parser.rb
+++ app/models/concerns/input_parser.rb
@@ -3,6 +3,7 @@ module InputParser
 
   def self.normalize_input input , space_sensitive = false
     unless space_sensitive
+      input ||= ""
       # remove all carriage returns and backspace and tab first
       arr_of_lines = input.gsub(/[\r\b\t]+/ , "").split("\n").map! { |line| line.gsub(/\s+/ , " ").strip }
       arr_of_lines.reject! { |line| line.empty? }

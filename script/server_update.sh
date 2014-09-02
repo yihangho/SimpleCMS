d556f054eff1a268e84976cdc062514775acb4f4
Yihang Ho <hoyihang5@gmail.com>
2014-09-02 11:27:39 +0800

    Fix the update script to properly detect pid file.

--- script/server_update.sh
+++ script/server_update.sh
@@ -19,7 +19,7 @@ RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rake assets:clean
 
 echo "Restart server"
-if [[ -f tmp/pids/thin.pid ]]; then
+if [[ -f tmp/pids/thin.0.pid ]]; then
   RAILS_ENV=production bundle exec thin --config config/thin.yml restart
 else
   RAILS_ENV=production bundle exec thin --config config/thin.yml start

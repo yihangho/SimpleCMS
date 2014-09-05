f9be2157efb686c1e0010ca06192079d522266a5
Yihang Ho <hoyihang5@gmail.com>
2014-09-05 12:10:26 +0800

    Fix typo in bootstrap script.

--- script/server_bootstrap.sh
+++ script/server_bootstrap.sh
@@ -10,7 +10,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 echo "Installing system dependencies"
-sudo apt-get -y install git-core postgresql libpq-dev nodejs openssh-server nginx redis
+sudo apt-get -y install git-core postgresql libpq-dev nodejs openssh-server nginx redis-server
 
 echo "Installing RVM"
 \curl -sSL https://get.rvm.io | bash -s stable

969521863c137b52a303840cabef8f32a1d37fc9
Yihang Ho <hoyihang5@gmail.com>
2014-10-20 14:32:54 +0800

    Read site title from environment variable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       datetime.strftime("%b %-d, %Y %l:%M %p %z")
     end
   end
+
+  def site_title
+    if ENV["SIMPLECMS_TITLE"] && !ENV["SIMPLECMS_TITLE"].empty?
+      ENV["SIMPLECMS_TITLE"]
+    else
+      "SimpleCMS"
+    end
+  end
 end

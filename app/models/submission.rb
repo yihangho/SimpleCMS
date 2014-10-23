95108b95c1f1175ec3e01c6115709f56ee339abe
Yihang Ho <hoyihang5@gmail.com>
2014-10-24 00:42:34 +0800

    Update the Submission before/after save hooks.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -8,23 +8,37 @@ class Submission < ActiveRecord::Base
 
   validates :user_id, :task_id, :presence => true
 
-  after_create do
-    regrade
+  before_save do
+    self.accepted = task.grade(input, user)
 
-    if accepted
-      task.solvers << user unless task.solvers.include?(user)
+    true
+  end
 
-      if !task.problem.solved_by?(user) && task.problem.tasks.all? { |task| task.solvers.include?(user) }
-        task.problem.solvers << user
-      end
+  after_save do
+    solve_status = user.solve_statuses.where(:problem_id => task.problem).first
+
+    unless solve_status
+      solve_status = user.solve_statuses.create(:problem_id => task.problem.id)
     end
-  end
 
-  def correct_input?
-    task.grade(input, user)
-  end
+    # As of Rails 4.1.6, it is necessary to make a copy of .tasks and then copy it back
+    # by either using solve_status.tasks = tasks or .update_attribute.
+    # The reason behind is that ActiveModel tracks changes by rigging the attribute writer,
+    # but doing .tasks[something] = something does not go through the writer, hence,
+    # ActiveModel will not know that .tasks is changed. As a result, doing a
+    # .save or .update_attribute without doing .tasks.dup will not update .tasks.
+    #
+    # TODO Check if this happens in Rails 4.2
+    tasks = solve_status.tasks.dup
+    tasks[task.id] ||= []
 
-  def regrade
-    update_attribute(:accepted, correct_input?)
+    if accepted?
+      tasks[task.id] << id unless tasks[task.id].include?(id)
+    else
+      tasks[task.id].delete(id)
+      tasks[task.id] = false if tasks[task.id].empty?
+    end
+
+    solve_status.update_attribute(:tasks, tasks)
   end
 end

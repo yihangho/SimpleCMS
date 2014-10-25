e36122078245e4d2d23947aaebf8bbf2eafb4e87
Yihang Ho <hoyihang5@gmail.com>
2014-10-25 16:07:10 +0800

    Add before_save callback to ContestResult to calculate total score.

--- app/models/contest_result.rb
+++ app/models/contest_result.rb
@@ -1,4 +1,16 @@
 class ContestResult < ActiveRecord::Base
   belongs_to :user
   belongs_to :contest
+
+  before_save do
+    new_total = 0
+
+    scores.each do |_, tasks|
+      new_total += Task.find(tasks.select { |_, v| v }.keys).inject(0) do |sum, task|
+        sum + task.point
+      end
+    end
+
+    self.total_score = new_total
+  end
 end

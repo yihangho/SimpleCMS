ec92645d6a9a2eb3f73374ae2052ab2cac834055
fyquah95 <quah.fy95@gmail.com>
2015-07-04 03:42:54 +0100

    Retrieve GPG Key to install RVM

--- script/server_bootstrap.sh
+++ script/server_bootstrap.sh
@@ -13,6 +13,7 @@ echo "Installing system dependencies"
 sudo apt-get -y install git-core postgresql libpq-dev nodejs openssh-server nginx redis-server
 
 echo "Installing RVM"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
